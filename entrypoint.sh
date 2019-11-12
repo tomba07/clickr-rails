9c6b0c3b3700a012880efdf0383935986a633b97
Fredrik Teschke <f@ftes.de>
2019-11-12 13:41:55 +0100

    :wrench: Switch to multi-stage docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -e
+set -ex
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /usr/src/app/tmp/pids/server.pid
 
-bundle exec rails db:create db:migrate
+bundler exec rails db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
