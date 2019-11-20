bdbcf59e420b466be9c65cff77750eccad55e5f7
Fredrik Teschke <f@ftes.de>
2019-11-20 21:56:19 +0100

    :recycle: Prefer redirect_back over redirect_to referer

--- services/web/app/controllers/users_controller.rb
+++ services/web/app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def school_class
     # TODO Move under /users/<id>/school_class for RESTful API
     current_user&.update_attribute(:school_class_id, params[:school_class_id])
-    redirect_to request.referer if request.referer
+    redirect_back fallback_location: root_path, notice: t('.notice')
   end
 end
