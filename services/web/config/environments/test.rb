84e40e4b1943bad8687c54a5e1d61dae00a5fe8b
Fredrik Teschke <f@ftes.de>
2019-11-21 23:03:55 +0100

    :wrench: Disable assset pipeline for tests (do not require yarn/bulma)

--- services/web/config/environments/test.rb
+++ services/web/config/environments/test.rb
@@ -46,6 +46,9 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
+  # Disable asset pipeline for tests
+  config.assets.enabled = false
+
   # create associations when building (not saving) parent
   FactoryBot.use_parent_strategy = false
 end
