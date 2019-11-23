ce3c5264f7a1208209e9e0af49425f80ec4defcf
Fredrik Teschke <f@ftes.de>
2019-11-23 20:57:53 +0100

    :bug: Fix tests

--- services/web/test/controllers/buttons_controller_test.rb
+++ services/web/test/controllers/buttons_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class ButtonsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = create(:user)
+    sign_in @user
+  end
+
   test "should get index" do
-    get buttons_index_url
+    get buttons_url
     assert_response :success
   end
 
