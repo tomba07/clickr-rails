4c15b44f5f237f09fc4b4439b34b5a9bcd1aaf6a
Fredrik Teschke <f@ftes.de>
2019-11-29 00:12:04 +0100

    :wrench: Create and seed db on docker on first run

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -4,7 +4,12 @@ set -ex
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-bundle exec rails db:migrate
+if bundle exec rails db:exists; then
+  bundle exec rails db:migrate
+else
+  bundle exec rails db:create db:migrate db:seed
+fi
+
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
